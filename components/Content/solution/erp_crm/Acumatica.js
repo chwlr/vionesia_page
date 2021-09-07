aec02360103bab525065dd1e353de1bff3f49788
chwlr <ch.williamr@gmail.com>
2021-09-07 10:14:30 +0700

    update component content solutions

--- components/Content/solution/erp_crm/Acumatica.js
+++ components/Content/solution/erp_crm/Acumatica.js
@@ -1,53 +1,57 @@
 import React from 'react'
-import { Row, Col, Card } from 'react-bootstrap'
+import {Row, Col, Card, Container} from 'react-bootstrap'
 import { Card_styled } from '../style'
 
 export default function Acimatica () {
   return (
-    <Row id="acumatica" className="my-5 transform-right-left">
+    <Container id="acumatica" className="my-5 transform-right-left">
             <Row>
-              <Col><h4 className="bold text-center py-5" style={{ color: 'black' }}>Acumatica Cloud ERP Software</h4></Col>
+              <Col className="justify-content-center align-items-center">
+                  <h4 className="bold text-center py-5" style={{ color: 'black' }}>Acumatica Cloud ERP Software</h4>
+              </Col>
             </Row>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">General Business Edition</p>
-                  <p className="p-regular">One complete package to run your business: Financials, Project Accounting, CRM, and Reporting and BI.</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">Distribution Edition</p>
-                  <p className="p-regular">Manage quotes and orders, track inventory, automate purchasing, and improve customer service. Acumatica Distribution Management is fully integrated with CRM, financials, manufacturing and project accounting for visibility across the entire organization.</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">Manufacturing Edition</p>
-                  <p className="p-regular">Integrate production planning, material purchasing, and shop floor scheduling with customer management, sales orders, inventory, purchasing, and accounting for real-time coordination of multi-plant activities. Use phones and tablets to capture material issues, move transactions, and labor with Acumatica’s mobile app and scanning.</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">Construction Edition</p>
-                  <p className="p-regular">Easily manage customers, finances—including job cost accounting—field and service teams in one complete solution. Increase visibility, team collaboration, and profitability on every project.</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">Retail-Commerce Edition</p>
-                  <p className="p-regular">Deliver a consistent customer experience across all your sales channels with full integration between your online, mobile, kiosk, and in-store service.</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-          </Row>
+            <Row>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">General Business Edition</p>
+                            <p className="p-regular">One complete package to run your business: Financials, Project Accounting, CRM, and Reporting and BI.</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">Distribution Edition</p>
+                            <p className="p-regular">Manage quotes and orders, track inventory, automate purchasing, and improve customer service. Acumatica Distribution Management is fully integrated with CRM, financials, manufacturing and project accounting for visibility across the entire organization.</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">Manufacturing Edition</p>
+                            <p className="p-regular">Integrate production planning, material purchasing, and shop floor scheduling with customer management, sales orders, inventory, purchasing, and accounting for real-time coordination of multi-plant activities. Use phones and tablets to capture material issues, move transactions, and labor with Acumatica’s mobile app and scanning.</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">Construction Edition</p>
+                            <p className="p-regular">Easily manage customers, finances—including job cost accounting—field and service teams in one complete solution. Increase visibility, team collaboration, and profitability on every project.</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">Retail-Commerce Edition</p>
+                            <p className="p-regular">Deliver a consistent customer experience across all your sales channels with full integration between your online, mobile, kiosk, and in-store service.</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+            </Row>
+          </Container>
   )
 }
--- components/Content/solution/erp_crm/Oracle.js
+++ components/Content/solution/erp_crm/Oracle.js
@@ -1,39 +1,41 @@
 import React from 'react'
-import { Row, Col, Card } from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import { Card_styled } from '../style'
 
 export default function Oracle () {
   return (
-    <Row id="oracle" className="my-5 transform-right-left">
+    <Container id="oracle" className="my-5 transform-right-left">
             <Row>
-              <Col>
-                <h4 className="bold text-center py-5" style={{ color: 'black' }}>Oracle Cloud Application</h4>
-              </Col>
+                <Col className="justify-content-center align-items-center">
+                    <h4 className="bold text-center py-5" style={{ color: 'black' }}>Oracle Cloud Application</h4>
+                </Col>
             </Row>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">Fusion Application Suite</p>
-                  <p className="p-regular">Oracle Fusion Cloud Applications is the world’s most complete cloud applications suite. Engineered to work together and with a modern user experience, Fusion Cloud Applications delivers continuous innovation via quarterly updates across every business function: finance, human resources, supply chain, manufacturing, sales, customer service, and marketing.</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">NetSuite Application</p>
-                  <p className="p-regular">NetSuite built the first unified suite of cloud applications that companies can use to run their entire business – all in one place. Organizations around the world use NetSuite to more easily grow and adapt to change.</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-            <Col xs="12" md="4" lg="4">
-              <Card_styled>
-                <Card.Body>
-                  <p className="p-lead">Industry-Specific Applications</p>
-                  <p className="p-regular">We understand your industry is unlike any other. Only Oracle offers the deep industry domain expertise and hundreds of industry cloud solutions you need to meet your most complex operational, regulatory and customer challenges</p>
-                </Card.Body>
-              </Card_styled>
-            </Col>
-          </Row>
+            <Row>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">Fusion Application Suite</p>
+                            <p className="p-regular">Oracle Fusion Cloud Applications is the world’s most complete cloud applications suite. Engineered to work together and with a modern user experience, Fusion Cloud Applications delivers continuous innovation via quarterly updates across every business function: finance, human resources, supply chain, manufacturing, sales, customer service, and marketing.</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">NetSuite Application</p>
+                            <p className="p-regular">NetSuite built the first unified suite of cloud applications that companies can use to run their entire business – all in one place. Organizations around the world use NetSuite to more easily grow and adapt to change.</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+                <Col xs="12" md="4" lg="4">
+                    <Card_styled>
+                        <Card.Body>
+                            <p className="p-lead">Industry-Specific Applications</p>
+                            <p className="p-regular">We understand your industry is unlike any other. Only Oracle offers the deep industry domain expertise and hundreds of industry cloud solutions you need to meet your most complex operational, regulatory and customer challenges</p>
+                        </Card.Body>
+                    </Card_styled>
+                </Col>
+            </Row>
+          </Container>
   )
 }
