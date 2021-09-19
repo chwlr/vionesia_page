50990280d048f3572e3c4740f566a25ec183d2a6
Chris-vionesia <chris-vionesia@192.168.1.25>
2021-09-19 15:42:56 +0700

    update home content

--- components/Content/home/index.js
+++ components/Content/home/index.js
@@ -9,25 +9,24 @@ export default function HomeContent () {
                  <Row className="mx-auto transform-right-left ">
                      <Column_styled md={6} lg={6} >
                          <h4 className="bold" >System Integrator</h4>
-                         <p className="p-lead">ERP, CRM, HR Cloud Solution</p>
+                         <p className="p-lead">End-to-end of enterprize system implementation for enterprise scale to medium scale organization</p>
                          <Link href="/products/erp-crm-and-hr"><Button className="btn-dark btn-md rounded-pill">Details</Button></Link>
                      </Column_styled>
                      <Column_styled md={6} lg={6} >
                          <h4 className="bold" >Professional Services</h4>
-                         <p className="p-lead">Front End-Developer, Back-End Developer, Full Stack
-                             Developer, System Analyst, Quality Assurance Analysis</p>
+                         <p className="p-lead">Vionesia provide IT Professionals to helo customer manage their specific area of businesses</p>
                          <Link href="/products/professional-services"><Button className="btn-dark btn-md rounded-pill">Details</Button></Link>
                      </Column_styled>
                  </Row>
                  <Row className="mx-auto transform-left-right ">
                      <Column_styled md={6} lg={6} >
-                         <h4 className="bold" >Defence & Security</h4>
-                         <p className="p-lead">Unmanned Technologies, Anti-Drone Technologies, Facial Recognition, Cyber Security, Social Media Analytics </p>
+                         <h4 className="bold" >Tactical Equipment Supplier</h4>
+                         <p className="p-lead">Vioensia has teamed up with several international principals in the area of defence and tactical equipment provider to contribute in the defence and general other industry</p>
                          <Link href="/products/security-and-defence"><Button className="btn-dark btn-md rounded-pill">Details</Button></Link>
                      </Column_styled>
                      <Column_styled md={6} lg={6} >
                          <h4 className="bold" >Technology Enablement</h4>
-                         <p className="p-lead">Cloud Migration & Deployment, Complex integrations, Hardware and sensor integration</p>
+                         <p className="p-lead">Provide complex integration and deployment of technology platform, front-end and back-end technology platform</p>
                          <Link href="/products/technology-enablement"><Button className="btn-dark btn-md rounded-pill">Details</Button></Link>
                      </Column_styled>
                  </Row>
